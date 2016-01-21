ada0c847f05dcce86d0cdc94c24882d7838683e6
Elias Mårtenson <lokedhs@gmail.com>
2016-01-22 00:53:59 +0800

    Added an action bar button that will be used to control the user list

--- app/src/main/java/com/dhsdevelopments/potato/channelmessages/ChannelContentActivity.java
+++ app/src/main/java/com/dhsdevelopments/potato/channelmessages/ChannelContentActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import com.dhsdevelopments.potato.R;
@@ -37,15 +38,15 @@ public class ChannelContentActivity extends AppCompatActivity implements HasUser
         Toolbar toolbar = (Toolbar)findViewById( R.id.detail_toolbar );
         setSupportActionBar( toolbar );
 
-        FloatingActionButton fab = (FloatingActionButton)findViewById( R.id.fab );
-        fab.setOnClickListener( new View.OnClickListener()
-        {
-            @Override
-            public void onClick( View view ) {
-                Snackbar.make( view, "Replace with your own detail action", Snackbar.LENGTH_LONG )
-                        .setAction( "Action", null ).show();
-            }
-        } );
+//        FloatingActionButton fab = (FloatingActionButton)findViewById( R.id.fab );
+//        fab.setOnClickListener( new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick( View view ) {
+//                Snackbar.make( view, "Replace with your own detail action", Snackbar.LENGTH_LONG )
+//                        .setAction( "Action", null ).show();
+//            }
+//        } );
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
@@ -91,6 +92,12 @@ public class ChannelContentActivity extends AppCompatActivity implements HasUser
                                        .add( R.id.channel_detail_container, channelContentFragment )
                                        .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ) {
+        getMenuInflater().inflate( R.menu.channel_content_toolbar_menu, menu );
+        return true;
     }
 
     public ChannelUsersTracker getUsersTracker() {
