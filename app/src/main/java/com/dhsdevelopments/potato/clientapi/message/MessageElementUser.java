4065fcd0080920228d4967b4d875bf03f8dc9245
Elias Mårtenson <lokedhs@gmail.com>
2016-01-24 19:10:08 +0800

    Render user names in a different background colour

--- app/src/main/java/com/dhsdevelopments/potato/clientapi/message/MessageElementUser.java
+++ app/src/main/java/com/dhsdevelopments/potato/clientapi/message/MessageElementUser.java
@@ -1,5 +1,10 @@
 package com.dhsdevelopments.potato.clientapi.message;
 
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.BackgroundColorSpan;
+
 public class MessageElementUser extends MessageElement
 {
     private String userId;
@@ -12,6 +17,8 @@ public class MessageElementUser extends MessageElement
 
     @Override
     public CharSequence getSpannable() {
-        return userDescription;
+        Spannable s = new SpannableString( userDescription );
+        s.setSpan( new BackgroundColorSpan( Color.rgb( 0xe3, 0xe3, 0xe3 ) ), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE );
+        return s;
     }
 }
