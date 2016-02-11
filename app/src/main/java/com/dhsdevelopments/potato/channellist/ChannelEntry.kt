b9383234a5bc764598dafc22e98c02dec4cb8963
Elias Martenson <lokedhs@gmail.com>
2016-02-11 14:15:04 +0800

    Converted ChannelEntry to Kotlin

---
+++ app/src/main/java/com/dhsdevelopments/potato/channellist/ChannelEntry.kt
@@ -0,0 +1,7 @@
+package com.dhsdevelopments.potato.channellist
+
+class ChannelEntry internal constructor(
+        val id: String,
+        val name: String,
+        val isPrivateChannel: Boolean,
+        val unread: Int)
