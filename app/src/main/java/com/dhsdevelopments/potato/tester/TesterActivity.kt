edc0a8e35a4b68a9cf5950f72613b0afaaaa348b
Elias Martenson <lokedhs@gmail.com>
2016-02-25 16:24:04 +0800

    Implement some test code for dialogs

--- app/src/main/java/com/dhsdevelopments/potato/tester/TesterActivity.kt
+++ app/src/main/java/com/dhsdevelopments/potato/tester/TesterActivity.kt
@@ -1,22 +1,95 @@
 package com.dhsdevelopments.potato.tester
 
+import android.app.Activity
+import android.app.DialogFragment
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.NumberPicker
+import com.dhsdevelopments.potato.Log
 import com.dhsdevelopments.potato.R
 import com.dhsdevelopments.potato.nlazy
 
-class TesterActivity : AppCompatActivity() {
-    val testNotificationButton: Button by nlazy { findViewById(R.id.test_notification_button) as Button }
+class TesterActivity : Activity() {
+    val testNotificationButton by nlazy { findViewById(R.id.test_notification_button) as Button }
+    val openDialogButton by nlazy { findViewById(R.id.test_dialog_button) as Button }
+    val dialogStylePicker by nlazy { findViewById(R.id.dialog_style_number_picker) as NumberPicker }
+    val themePicker by nlazy { findViewById(R.id.theme_picker) as NumberPicker }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tester)
 
         testNotificationButton.setOnClickListener { testNotification() }
+        openDialogButton.setOnClickListener { openDialog() }
+
+        dialogStylePicker.minValue = 0
+        dialogStylePicker.maxValue = 7
+        themePicker.minValue = 0
+        themePicker.maxValue = 4
     }
 
     private fun testNotification() {
 
+    }
+
+    private fun openDialog() {
+        val ft = fragmentManager.beginTransaction()
+        val prev = fragmentManager.findFragmentByTag("dialog")
+        if (prev != null) {
+            ft.remove(prev)
+        }
+        ft.addToBackStack(null)
+
+        val dialogFragment = TestDialogFragment()
+        val args = Bundle()
+        args.putInt("style", makeStyleFromInt(dialogStylePicker.value))
+        args.putInt("theme", makeThemeFromInt(themePicker.value))
+        Log.d("Setting arguments to: $args")
+        dialogFragment.arguments = args
+        dialogFragment.show(ft, "dialog")
+    }
+
+    private fun makeStyleFromInt(style: Int): Int {
+        return when (style) {
+            0 -> DialogFragment.STYLE_NO_TITLE
+            1 -> DialogFragment.STYLE_NO_FRAME
+            2 -> DialogFragment.STYLE_NO_INPUT
+            3 -> DialogFragment.STYLE_NORMAL
+            4 -> DialogFragment.STYLE_NORMAL
+            5 -> DialogFragment.STYLE_NO_TITLE
+            6 -> DialogFragment.STYLE_NO_FRAME
+            7 -> DialogFragment.STYLE_NORMAL
+            else -> throw IllegalArgumentException("Illegal style id: $style")
+        }
+    }
+
+    private fun makeThemeFromInt(theme: Int): Int {
+        return when (theme) {
+            0 -> android.R.style.Theme_Material
+            1 -> android.R.style.Theme_Material_Dialog
+            2 -> android.R.style.Theme_Material_Dialog_Presentation
+            3 -> android.R.style.Theme_Material_Panel
+            4 -> android.R.style.Theme_Material_Light_Panel
+            else -> throw IllegalArgumentException("Illegal theme id: $theme")
+        }
+    }
+}
+
+class TestDialogFragment : DialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val styleId = arguments.getInt("style")
+        val themeId = arguments.getInt("theme")
+        setStyle(styleId, themeId)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_test_dialog, container, false)
+        dialog.setTitle("Test title")
+        return view;
     }
 }
