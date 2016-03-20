4b73c77ae957eba4442fc514075bac596dc82628
Elias Mårtenson <lokedhs@gmail.com>
2016-03-20 15:39:04 +0800

    Kotlin lambda functions test code

--- app/src/main/java/com/dhsdevelopments/potato/tester/KtTest.kt
+++ app/src/main/java/com/dhsdevelopments/potato/tester/KtTest.kt
@@ -1,15 +1,35 @@
 package com.dhsdevelopments.potato.tester
 
-import com.dhsdevelopments.potato.makeRandomCharacterSequence
+class Foo(val value: Int) {
+    override fun toString(): String{
+        return "Foo(value=$value)"
+    }
+}
+
+class FooResult(val value: Int) {
+    override fun toString(): String{
+        return "FooResult(value=$value)"
+    }
+}
+
+infix fun (() -> Foo).bar(fn: () -> Foo): FooResult {
+    val v1 = this()
+    val v2 = fn()
+    return FooResult(v1.value + v2.value)
+}
+
+fun computeResult(fn: () -> FooResult): FooResult {
+    return fn()
+}
 
 class KtTest {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val buf = StringBuilder()
-            buf.append("koko")
-            makeRandomCharacterSequence(buf, 5)
-            println("buf = '$buf'")
+            val v1 = Foo(1)
+            val v2 = Foo(100)
+            val v3 = computeResult { {v1} bar {v2} }
+            println("v3=$v3")
         }
     }
 }
